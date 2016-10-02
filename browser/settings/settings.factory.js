c9c7404a9d54dfbce19e386542202c3e32e18f40
Will Livengood <wlivengo@gmail.com>
2016-10-02 12:55:49 -0400

    settings

--- browser/settings/settings.factory.js
+++ browser/settings/settings.factory.js
@@ -3,10 +3,6 @@ angular.module('settingsService', function($http) {
 	return {
 		fetchAll: function() {
 			return $http.get('/api/groups');
-		},
-
-		changeGroup: function(user, group) {
-			return $http.put('/api/users/' + user.id + '/group', group)
 		}
 	}
 

6c576b2c028ce0424cc9fc51157d10dcf35a4095
Will Livengood <wlivengo@gmail.com>
2016-10-02 13:38:18 -0400

    otherwise

--- browser/app.js
+++ browser/app.js
@@ -1,5 +1,7 @@
 angular.module('app', ['ui.router'])
 	.config(function($stateProvider, $urlRouterProvider) {
+		$urlRouterProvider.otherwise('/');
+
 		$stateProvider
 			.state('home', {
 				url: '/',

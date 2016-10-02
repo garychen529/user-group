191ec3736475e26f817f734f674a1461fdd7bf39
Will Livengood <wlivengo@gmail.com>
2016-10-02 13:20:17 -0400

    added settings functionality

--- browser/settings/settings.factory.js
+++ browser/settings/settings.factory.js
@@ -1,8 +1,17 @@
 angular.module('settingsService', function($http) {
 
+	var _group = {};
+
 	return {
 		fetchAll: function() {
 			return $http.get('/api/groups');
+		},
+
+		updateGroup: function(group) {
+			return $http.put('/api/groups', group.id)
+			.then(function(result) {
+				angular.copy(results.data, _group);
+			});
 		}
 	}
 
